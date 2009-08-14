20de3f947c062bb0a555194c906ebf08d3f953c2
oliver <oliver@Big-Mac.local>
2009-08-14 08:17:13 +0800

    adding a Pages tab to the admin, and removing the Pages link from the configuration section
    Signed-off-by: Peter Berkenbosch <peterberkenbosch@me.com>

--- static_content_extension.rb
+++ static_content_extension.rb
@@ -8,17 +8,31 @@ class StaticContentExtension < Spree::Extension
 
   def activate
     
-    Admin::ConfigurationsController.class_eval do
-      before_filter :add_static_pages_links, :only => :index
-      
-      def add_static_pages_links
-        @extension_links << {
-          :link => admin_pages_path,
-          :link_text => t('ext_static_content_static_pages'),
-          :description => t('ext_static_content_static_pages_desc')
-        }
+    # add Pages tab
+    Admin::BaseController.class_eval do
+      before_filter :add_static_pages_tab
+
+      def add_static_pages_tab
+        # @extension_tabs << [ :pages, {
+         #                        :link => admin_pages_path,
+         #                        :link_text => t('ext_static_content_static_pages'),
+         #                        :description => t('ext_static_content_static_pages_desc')
+         #                      } ]
+          @extension_tabs <<  [ :pages ]
       end
     end
+    
+    # Admin::ConfigurationsController.class_eval do
+    #   before_filter :add_static_pages_links, :only => :index
+    #   
+    #   def add_static_pages_links
+    #     @extension_links << {
+    #       :link => admin_pages_path,
+    #       :link_text => t('ext_static_content_static_pages'),
+    #       :description => t('ext_static_content_static_pages_desc')
+    #     }
+    #   end
+    # end
 
     Spree::BaseController.class_eval do
       
