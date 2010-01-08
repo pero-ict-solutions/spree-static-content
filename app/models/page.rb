6aca0a511c82a2e64123dd7ed34406d2f482e2c7
Marcin Raczkowski <marcin.raczkowski@gmail.com>
2010-01-08 17:15:09 +0800

    Automatic position setting and reordering of position

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,10 +1,29 @@
 class Page < ActiveRecord::Base
+  default_scope :order => "position ASC"
+
   validates_presence_of :title
   validates_presence_of [:slug, :body], :if => :not_using_foreign_link?
   
   named_scope :header_links, :conditions => ["show_in_header = ?", true], :order => 'position'
   named_scope :footer_links, :conditions => ["show_in_footer = ?", true], :order => 'position'
   named_scope :visible, :conditions => {:visible => true}
+
+  def initialize(*args)
+    super(*args)
+    last_page = Page.last
+    self.position = last_page ? last_page.position + 1 : 0
+  end
+
+  def before_save
+    unless new_record?
+      return unless prev_position = Page.find(self.id).position
+      if prev_position > self.position
+        Page.update_all("position = position + 1", ["? <= position AND position < ?", self.position, prev_position])
+      elsif prev_position < self.position
+        Page.update_all("position = position - 1", ["? < position AND position <= ?", prev_position,  self.position])
+      end
+    end
+  end
 
   def link
     foreign_link.blank? ? slug_link : foreign_link
