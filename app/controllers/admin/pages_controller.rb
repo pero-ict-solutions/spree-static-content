968afe1ad9178e375278a09759fa3fa558b70b55
Roman Smirnov <roman@railsdog.com>
2011-06-25 18:51:11 +0400

    Fixed strange issue with incorrect initial value of @page

--- app/controllers/admin/pages_controller.rb
+++ app/controllers/admin/pages_controller.rb
@@ -1,8 +1,16 @@
 class Admin::PagesController < Admin::ResourceController
   update.after :expire_cache
   
+  def new
+    @page = @object
+  end
+  
+  def edit
+    @page = @object
+  end
+    
   private
   def expire_cache
-    expire_page :controller => '/static_content', :action => 'show', :path => @page.slug
+    expire_page :controller => '/static_content', :action => 'show', :path => @object.slug
   end
 end
