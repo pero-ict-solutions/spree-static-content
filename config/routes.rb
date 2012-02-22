c3d3c4ff6d39e766ea11624e4729343f54347a40
Peter Berkenbosch <peter@pero-ict.nl>
2012-02-22 20:42:34 +0100

    use prepend instead of append to make sure we can override spree pages.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
-Spree::Core::Engine.routes.append do
-
+Spree::Core::Engine.routes.prepend do
   namespace :admin do
     resources :pages
   end
-
-  match '/static/*path', :to => 'static_content#show', :via => :get, :as => 'static'
-
+  
   match '/*path', :to => 'static_content#show', :via => :get, :as => 'static'
-
 end
