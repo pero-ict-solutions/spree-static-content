323ff25a29f72ee3c544e186748f24813dbf574a
Peter Berkenbosch <peter@pero-ict.nl>
2012-02-22 22:41:43 +0100

    revert some, since the prepend was breaking all other dynamic pages.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
-Spree::Core::Engine.routes.prepend do
+Spree::Core::Engine.routes.append do
+
   namespace :admin do
     resources :pages
   end
   
+  match '/static/*path', :to => 'static_content#show', :via => :get, :as => 'static'
   match '/*path', :to => 'static_content#show', :via => :get, :as => 'static'
 end
--- lib/spree_static_content/engine.rb
+++ lib/spree_static_content/engine.rb
@@ -1,12 +1,11 @@
 module SpreeStaticContent
   class Engine < Rails::Engine
     engine_name 'spree_static_content'
-
+    
     def self.activate
       Dir.glob(File.join(File.dirname(__FILE__), "../../app/**/*_decorator*.rb")) do |c|
         Rails.application.config.cache_classes ? require(c) : load(c)
       end
-
       Dir.glob(File.join(File.dirname(__FILE__), "../../app/overrides/**/*.rb")) do |c|
         Rails.application.config.cache_classes ? require(c) : load(c)
       end
