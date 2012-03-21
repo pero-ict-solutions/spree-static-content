67dc5d7efac85df9ec645236f5f6d65d5deb7225
Peter Berkenbosch <peter@pero-ict.nl>
2012-03-21 11:23:27 +0100

    moved gsub before the remove mount point conditional to account for //
    Signed-off-by: Peter Berkenbosch <peter@pero-ict.nl>

--- app/controllers/spree/static_content_controller.rb
+++ app/controllers/spree/static_content_controller.rb
@@ -11,8 +11,9 @@ class Spree::StaticContentController < Spree::BaseController
     when nil
       request.path
     end
-    path = StaticPage::remove_spree_mount_point(path) unless Rails.application.routes.named_routes[:spree].path.spec.to_s == "/"
     path = path.gsub('//','/')
+    path = StaticPage::remove_spree_mount_point(path) unless Rails.application.routes.named_routes[:spree].path == "/"
+    
     unless @page = Spree::Page.visible.find_by_slug(path)
       render_404
     end
