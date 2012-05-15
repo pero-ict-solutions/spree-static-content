b6c7f48d67c68892417703cd2755beeab0370006
Peter Berkenbosch <peter@pero-ict.nl>
2012-05-15 10:47:23 +0200

    Merge branch 'develop' of https://github.com/JohanB/spree_static_content into JohanB-develop
    Conflicts:
    	config/locales/en.yml
    
    Signed-off-by: Peter Berkenbosch <peter@pero-ict.nl>

--- app/overrides/pages_in_sidebar.rb
+++ app/overrides/pages_in_sidebar.rb
@@ -1,4 +1,4 @@
-Deface::Override.new(:virtual_path => "spree/layouts/spree_application",
+Deface::Override.new(:virtual_path => "spree/home/index",
                      :name => "pages_in_sidebar",
-                     :insert_bottom => "#sidebar",
+                     :insert_bottom => "[data-hook='homepage_sidebar_navigation']",
                      :partial => "spree/static_content/static_content_sidebar")
