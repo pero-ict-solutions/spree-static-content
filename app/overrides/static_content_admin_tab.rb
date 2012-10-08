d90c5215995cc4eda62d9a5a6640656346c23e27
Johan Bruning <johan.h.bruning@gmail.com>
2012-10-08 22:21:02 +0200

    Changed admin tab to include an icon

--- app/overrides/static_content_admin_tab.rb
+++ app/overrides/static_content_admin_tab.rb
@@ -1,4 +1,4 @@
 Deface::Override.new(:virtual_path => "spree/layouts/admin",
                      :name => "static_content_admin_tab",
                      :insert_bottom => "[data-hook='admin_tabs']",
-                     :text => "<%= tab(:pages) %>")
+                     :text => "<%= tab(:pages, :icon => 'icon-file') %>")
