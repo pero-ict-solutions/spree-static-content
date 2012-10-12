ffc10857e746d8db6a0453ca2e0bcf9197ce78b2
Peter Berkenbosch <peter@pero-ict.nl>
2012-10-12 15:43:01 +0200

    remove spacing

--- lib/spree_static_content/engine.rb
+++ lib/spree_static_content/engine.rb
@@ -9,8 +9,6 @@ module SpreeStaticContent
       Dir.glob(File.join(File.dirname(__FILE__), "../../app/overrides/**/*.rb")) do |c|
         Rails.application.config.cache_classes ? require(c) : load(c)
       end
-
-
     end
     config.to_prepare &method(:activate).to_proc
     config.autoload_paths += %W(#{config.root}/lib)
