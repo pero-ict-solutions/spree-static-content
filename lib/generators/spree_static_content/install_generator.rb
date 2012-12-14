8b496af1aeec8d5e6c19c55515316db14932c9b5
Michael Bianco <iloveitaly@gmail.com>
2012-12-14 10:39:05 -0500

    Clearing out unnecessary code

--- lib/generators/spree_static_content/install_generator.rb
+++ lib/generators/spree_static_content/install_generator.rb
@@ -1,40 +1,17 @@
-if ::Rails.version < "3.1" || !::Rails.application.config.assets.enabled
-  module SpreeStaticContent
-    module Generators
-      class InstallGenerator < Rails::Generators::Base
-
-        def add_migrations
-          run 'rake railties:install:migrations FROM=spree_static_content'
-        end
-
-        def run_migrations
-           res = ask "Would you like to run the migrations now? [Y/n]"
-           if res == "" || res.downcase == "y"
-             run 'rake db:migrate'
-           else
-             puts "Skiping rake db:migrate, don't forget to run it!"
-           end
-        end
+module SpreeStaticContent
+  module Generators
+    class InstallGenerator < Rails::Generators::Base
+      def add_migrations
+        run 'rake railties:install:migrations FROM=spree_static_content'
       end
-    end
-  end
-else
-  module SpreeStaticContent
-    module Generators
-      class InstallGenerator < Rails::Generators::Base
-        def add_migrations
-          run 'rake railties:install:migrations FROM=spree_static_content'
-        end
 
-        def run_migrations
-           res = ask "Would you like to run the migrations now? [Y/n]"
-           if res == "" || res.downcase == "y"
-             run 'rake db:migrate'
-           else
-             puts "Skiping rake db:migrate, don't forget to run it!"
-           end
-        end
-
+      def run_migrations
+         res = ask "Would you like to run the migrations now? [Y/n]"
+         if res == "" || res.downcase == "y"
+           run 'rake db:migrate'
+         else
+           puts "Skiping rake db:migrate, don't forget to run it!"
+         end
       end
     end
   end
--- lib/spree_static_content/engine.rb
+++ lib/spree_static_content/engine.rb
@@ -6,12 +6,8 @@ module SpreeStaticContent
       Dir.glob(File.join(File.dirname(__FILE__), "../../app/**/*_decorator*.rb")) do |c|
         Rails.application.config.cache_classes ? require(c) : load(c)
       end
-      Dir.glob(File.join(File.dirname(__FILE__), "../../app/overrides/**/*.rb")) do |c|
-        Rails.application.config.cache_classes ? require(c) : load(c)
-      end
-
-
     end
+
     config.to_prepare &method(:activate).to_proc
     config.autoload_paths += %W(#{config.root}/lib)
   end
