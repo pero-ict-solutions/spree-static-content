67cd0f41109161399ebc525a87c182e46f325e9c
Greg Reinacker <gregr@rassoc.com>
2013-01-16 13:57:11 -0700

    fix issue with slug getting broken
    add spec for multiple-/ fix
    
    Conflicts:
    	spec/requests/static_content_spec.rb

--- spec/requests/static_content_spec.rb
+++ spec/requests/static_content_spec.rb
@@ -2,8 +2,22 @@ require 'spec_helper'
 
 describe 'Static Content Page' do
 
-  before do
-    Spree::Page.create(:slug => '/page', :title => 'Test Page', :body => 'Test page body')
+  it 'should render page with a slug not starting by /' do
+    Spree::Page.create(:slug => 'page2', :title => 'Test Page 2', :body => 'Test page body 2')
+    visit '/page2'
+    page.should have_content('Test page body 2')
+  end
+
+  it 'should render page with a slug with multiple /' do
+    Spree::Page.create(:slug => '/t/categories/page3', :title => 'Test Page 3', :body => 'Test page body 3')
+    visit '/t/categories/page3'
+    page.should have_content('Test page body 3')
+  end
+
+  it "should render a custom root page" do
+    Spree::Page.create(:slug => '/', :title => 'Root Page', :body => 'Root Body')
+    visit '/'
+    page.should have_content('Root Body')
   end
 
   it 'should render page when there is a query string' do
