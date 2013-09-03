1e7a19970e0dfddfdfe1b88e6fded2a9fc485048
Ryan Bigg <radarlistener@gmail.com>
2013-09-04 09:56:26 +1000

    Fix scopes + remove attr_accessible call in Page
    This is for Rails 4 compatibility

--- app/models/spree/page.rb
+++ app/models/spree/page.rb
@@ -1,5 +1,5 @@
 class Spree::Page < ActiveRecord::Base
-  default_scope :order => "position ASC"
+  default_scope -> { order("position ASC") }
 
   validates_presence_of :title
   validates_presence_of [:slug, :body], :if => :not_using_foreign_link?
@@ -8,14 +8,12 @@ class Spree::Page < ActiveRecord::Base
   validates :slug, :uniqueness => true, :if => :not_using_foreign_link?
   validates :foreign_link, :uniqueness => true, :allow_blank => true
 
-  scope :visible, where(:visible => true)
-  scope :header_links, where(:show_in_header => true).visible
-  scope :footer_links, where(:show_in_footer => true).visible
-  scope :sidebar_links, where(:show_in_sidebar => true).visible
+  scope :visible, -> { where(:visible => true) }
+  scope :header_links, -> { where(:show_in_header => true).visible }
+  scope :footer_links, -> { where(:show_in_footer => true).visible }
+  scope :sidebar_links, -> { where(:show_in_sidebar => true).visible }
 
   before_save :update_positions_and_slug
-
-  attr_accessible :title, :slug, :body, :meta_title, :meta_keywords, :meta_description, :layout, :foreign_link, :position, :show_in_sidebar, :show_in_header, :show_in_footer, :visible, :render_layout_as_partial
 
   def self.by_slug(slug)
     slug = StaticPage::remove_spree_mount_point(slug)
