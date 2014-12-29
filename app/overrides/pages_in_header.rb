42db3f49f9e528ff8f5c7f93101bfe3265e09060
Tobias Bohwalli <hi@futhr.io>
2014-12-29 14:56:01 +0100

    Rework frontend layout for master branch.

--- app/overrides/pages_in_header.rb
+++ app/overrides/pages_in_header.rb
@@ -1,5 +1,7 @@
-Deface::Override.new(:virtual_path => "spree/shared/_main_nav_bar",
-                     :name => "pages_in_header",
-                     :insert_bottom => "#main-nav-bar",
-                     :partial => "spree/static_content/static_content_header",
-                     :disabled => false)
+Deface::Override.new(
+  virtual_path: 'spree/shared/_main_nav_bar',
+  name: 'pages_in_header',
+  insert_bottom: '#main-nav-bar > ul:first-child',
+  partial: 'spree/static_content/static_content_header',
+  disabled: false
+)
