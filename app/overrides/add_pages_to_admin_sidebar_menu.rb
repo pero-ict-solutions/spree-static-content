3aefe4b89ed44eb640a0e493c4144d3cdbd350a8
Manuel Vidaurre <manuel.vidaurre@agiltec.com.mx>
2015-01-25 18:11:02 -0600

    Admin layout fixes.

--- app/overrides/add_pages_to_admin_sidebar_menu.rb
+++ app/overrides/add_pages_to_admin_sidebar_menu.rb
@@ -1,6 +1,6 @@
 Deface::Override.new(
   virtual_path: 'spree/layouts/admin',
   name: 'pages_admin_sidebar_menu',
-  insert_bottom: '.sidebar-menu',
+  insert_bottom: '#main-sidebar',
   partial: 'spree/admin/shared/pages_sidebar_menu'
 )
